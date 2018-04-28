956a9b506dabd0d94b68c47695245a9007e58948
Peter M Allan <peabrain@notatla.org.uk>
2018-04-28 20:15:46 +0100

    better reports of future OC

--- oc_history_to_text.py
+++ oc_history_to_text.py
@@ -10,7 +10,33 @@ class Crime_history:
     def __init__(self,docroot):
         self.docroot = docroot
 
-
+    def player_readiness(self, c, p_id, player_name, time_ready, t_end, t_begin, t_executed):
+        what_readiness = ''
+        t_first = None
+        c.execute("""select max(et) from readiness where player_id=? and et<? and et>?""",(p_id, t_end, t_begin,))
+        for player_history in c:
+            if player_history[0]:
+                t_first = player_history[0]
+        if t_first:
+            if t_executed:
+                t_end = t_executed
+            c.execute("""select et,cur_nerve,max_nerve,status_0,status_1  from readiness where player_id=? and et>=? and et<?""",(p_id, t_first, t_end,))
+            for player_history in c:
+                t_alibi,cur_nerve,max_nerve,status_0,status_1 = player_history
+                frog = dehtml.Dehtml()
+                status_0 = frog.html_clean(status_0)
+                status_1 = frog.html_clean(status_1)
+                delta_t = t_alibi - time_ready # could be +ve or -ve
+                what_readiness = what_readiness + '\n' + player_name +  ' ' +  time.strftime("%H:%M", time.gmtime(t_alibi)) + ' T '
+                if delta_t < 0:
+                    what_readiness += 'minus '
+                    delta_t *= -1
+                else:
+                    what_readiness += 'plus '
+                what_readiness = what_readiness + str(int(delta_t/60)) + 'm ' + status_0 + ' ' +  status_1
+                if max_nerve:
+                    what_readiness = what_readiness + ' nerve=' + str(cur_nerve) +'/'+ str(max_nerve)
+        return what_readiness
 
 
     #  db connection
@@ -21,12 +47,17 @@ class Crime_history:
     #        filename precursor
     #     player_id as p_id needed for status history
     def crime2html(self, c, db_queue, pid2name, need_status, source_type, dname, p_id, weekno):
+        now = time.time()
         # return value is [ success/failure, the HREF of the web page, mtime of web page ]
         how_recent = 0
+        time_ready = 0
+        time_executed = 0
         for row in db_queue:
             et=row[8]
             if et > how_recent:
                 how_recent = et
+                time_executed=row[4]
+                time_ready=row[9]
 
         if ('player' == source_type) and p_id:
             longdname = self.docroot  + 'player/' + dname
@@ -37,13 +68,15 @@ class Crime_history:
             rfname = hashlib.sha1(bytes('oc_history'  + dname, 'utf-8')).hexdigest()
             shortname = '/player/' + dname + '/' +  rfname + '.txt'
             longname =  self.docroot + shortname
-            try:
-                mtime = os.stat(longname).st_mtime
-                if int(mtime) > int(how_recent): # time-of-data
-                    # page exists, use it unchanged
-                    return [1, shortname, int(mtime)]
-            except:
-                pass # need to write file
+            if time_executed or ((time_ready - now) > 7200):
+                # might or might not write file
+                try:
+                    mtime = os.stat(longname).st_mtime
+                    if int(mtime) > int(how_recent): # time-of-data
+                        # page exists and is recent; use it unchanged
+                        return [1, shortname, int(mtime)]
+                except:
+                    pass # need to write file
         elif ('faction' == source_type):
             longdname = self.docroot + 'faction/' + dname
             try:
@@ -100,29 +133,8 @@ class Crime_history:
                     else:
                         print("WHY MISSING? ", p_id, " not in " , pid2name)
                         player_name = str(p_id)
-                    # Player readiness history goes here
-                    t_first = None
-                    c.execute("""select max(et) from readiness where player_id=? and et<? and et>?""",(p_id, time_ready, time_ready-3600,))
-                    for player_history in c:
-                        if player_history[0]:
-                            t_first = player_history[0]
-                    if t_first:
-                        c.execute("""select et,cur_nerve,max_nerve,status_0,status_1  from readiness where player_id=? and et>=? and et<?""",(p_id, t_first, time_executed,))
-                        for player_history in c:
-                            t_alibi,cur_nerve,max_nerve,status_0,status_1 = player_history
-                            frog = dehtml.Dehtml()
-                            status_0 = frog.html_clean(status_0)
-                            status_1 = frog.html_clean(status_1)
-                            delta_t = t_alibi - time_ready # could be +ve or -ve
-                            crimes_planned = crimes_planned + '\n' + player_name +  ' ' +  time.strftime("%H:%M", time.gmtime(t_alibi)) + ' T '
-                            if delta_t < 0:
-                                crimes_planned = crimes_planned + 'minus '
-                                delta_t *= -1
-                            else:
-                                crimes_planned = crimes_planned + 'plus '
-                            crimes_planned = crimes_planned + str(int(delta_t/60)) + 'm ' + status_0 + ' ' +  status_1
-                            if max_nerve:
-                                crimes_planned = crimes_planned + ' nerve=' + str(cur_nerve) +'/'+ str(max_nerve)
+                    # Player readiness history
+                    crimes_planned += self.player_readiness(c, p_id, player_name, time_ready, time_ready, time_ready-3600, time_executed)
                 #
                 if success:
                     crimes_planned = crimes_planned + '\n' + 'money=' + str(money) + ' respect=' + str(respect) 
@@ -138,7 +150,13 @@ class Crime_history:
                     delaytime =int(delaytime+0.5)
                     crimes_planned = crimes_planned +  ' delay=' + str(delaytime) + ' minutes'
             else:
-                crimes_planned = crimes_planned + '\nstill future'
+                t_remaining = int(time_ready - now)
+                if t_remaining > 0:
+                    crimes_planned = crimes_planned + '\nstill future ' +  time.strftime("%Y-%m-%d %H:%M", time.gmtime(time_ready))
+                else:
+                    crimes_planned = crimes_planned + '\ndue but not yet initiated'
+                # Player readiness history
+                crimes_planned += self.player_readiness(c, p_id, 'Advance readiness notice:', time_ready, now, now-86400, 0)
             crimes_planned = crimes_planned + '\n'
             linebreak='\n'
 
